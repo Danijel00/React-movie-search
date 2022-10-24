4ea8c53f828d30abba2c2da834fea3f522cb0b11
Danijel Kocić <79769638+Danijel00@users.noreply.github.com>
2022-10-24 18:22:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import searchIcon from './image/search.svg';
 import popcorn from './image/popcorn.png';
 import MovieCard from './MovieCard';
 
-const API_URL = 'http://www.omdbapi.com?apikey=e50ad83e';
+const API_URL = 'https://www.omdbapi.com?apikey=e50ad83e';
 
 // const movie = {
 //     "Title": "Spider-Man",
